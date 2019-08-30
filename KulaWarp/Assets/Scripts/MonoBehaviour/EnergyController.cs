5b3700447c87fef32abfc5d9130ddbe4660f445f
BahaMagi <hokanotame@protonmail.com>
2019-08-30 20:41:54 +0200

    Added proper EnergySphere effects

--- KulaWarp/Assets/Scripts/MonoBehaviour/EnergyController.cs
+++ KulaWarp/Assets/Scripts/MonoBehaviour/EnergyController.cs
@@ -5,26 +5,9 @@
 */
 public class EnergyController : PickUpController
 {
-    public float scaleAmount = 0.2f, scaleSpeed = 4.0f;
-
-    private Vector3 m_idleScale;
-
-    new void Awake()
-    {
-        base.Awake();
-
-        m_idleScale = transform.localScale;
-    }
-
     protected override void OnTriggerEnter(Collider other)
     {
         gameObject.SetActive(false);
         LevelController.lc.CollectEnergy();
-    }
-
-    protected override void AnimObject()
-    {
-        transform.localScale = m_idleScale + Vector3.one * (1 + Mathf.Sin(scaleSpeed * m_timeOffset)) * 0.5f * scaleAmount;
-        base.AnimObject();
     }
 }
