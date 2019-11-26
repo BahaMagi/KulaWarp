665c215d823d72adff99c23200db13838b333d34
BahaMagi <hokanotame@protonmail.com>
2019-11-26 17:47:53 +0100

    Added more BGM tracks.
    Added 2 more BGM tracks. The Audiocontroller will not loop but rather play the list and repeat the list.

--- KulaWarp/Assets/Scripts/MonoBehaviour/Controller/AudioController.cs
+++ KulaWarp/Assets/Scripts/MonoBehaviour/Controller/AudioController.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
     public static AudioController ac;
+
+    public AudioClip[] bgmPlayList;
+
+    private AudioSource m_audioSource;
+    private int m_currentClip = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -17,9 +20,23 @@ public class AudioController : MonoBehaviour
         else if (ac != this) Destroy(gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!m_audioSource.isPlaying && bgmPlayList.Length > 0)
+        {
+            PlayNextClip();
+        }
+    }
+
+    void PlayNextClip()
+    {
+        // Stop the playback incase the audio source is still playing
+        m_audioSource.Stop();
+
+        // Get the next clip from the play list
+        m_audioSource.clip = bgmPlayList[(m_currentClip + 1) % bgmPlayList.Length];
+
+        // Start the new playback
+        m_audioSource.Play();
     }
 }
