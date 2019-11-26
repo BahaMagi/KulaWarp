41b72cc2c44d0e959023f23ce436393eb681f8f5
BahaMagi <hokanotame@protonmail.com>
2019-11-26 17:29:06 +0100

    Scene persistant AudioController
    AudioCotroller is not destroyed when a new scene is loaded anymore. Thus, bgm is not interrupted.

---
+++ KulaWarp/Assets/Scripts/MonoBehaviour/Controller/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController ac;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (ac == null)
+        {
+            DontDestroyOnLoad(gameObject); // This object is scene persistent
+            ac = this;
+        }
+        else if (ac != this) Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
